87d1af63142aa9a4c490371aa98db67e6cd301a3
Marc Stammerjohann <marcstammerjohann@web.de>
2018-12-06 00:04:51 +0100

    chore(routing): empty routes to home page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,8 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'home', loadChildren: './pages/home/home.module#HomePageModule' },
+  { path: '', loadChildren: './pages/home/home.module#HomePageModule' },
   { path: '**', loadChildren: './pages/page-not-found/page-not-found.module#PageNotFoundPageModule' },
 ];
 
