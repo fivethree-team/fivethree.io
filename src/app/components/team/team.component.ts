00c1351abc30d9f68d21657f8c8f963eff24b90c
Marc Stammerjohann <marcstammerjohann@web.de>
2019-07-05 12:16:04 +0200

    feat(team): update marcs picture

--- src/app/components/team/team.component.ts
+++ src/app/components/team/team.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 export interface Member {
   name: string;
@@ -9,39 +9,37 @@ export interface Member {
 }
 
 @Component({
-  selector: 'app-team',
-  templateUrl: './team.component.html',
-  styleUrls: ['./team.component.scss']
+  selector: "app-team",
+  templateUrl: "./team.component.html",
+  styleUrls: ["./team.component.scss"]
 })
 export class TeamComponent implements OnInit {
-
   public team: Member[] = [
     {
-        name: 'Gary Großgarten',
-        picture: 'https://pbs.twimg.com/profile_images/823932144473018369/vGv7gtS__400x400.jpg',
-        description: 'UX Engineer',
-        twitter: 'https://twitter.com/garygrossgarten',
-        github: 'https://github.com/garygrossgarten'
+      name: "Gary Großgarten",
+      picture:
+        "https://pbs.twimg.com/profile_images/823932144473018369/vGv7gtS__400x400.jpg",
+      description: "UX Engineer",
+      twitter: "https://twitter.com/garygrossgarten",
+      github: "https://github.com/garygrossgarten"
     },
     {
-        name: 'Frank Thielen',
-        picture: 'https://frankthielen.de/assets/img/profile_quadrat.png',
-        description: 'Software Architect & Backend Developer',
-        twitter: 'https://twitter.com/frank_thi',
-        github: 'https://github.com/frathi'
+      name: "Frank Thielen",
+      picture: "https://frankthielen.de/assets/img/profile_quadrat.png",
+      description: "Software Architect & Backend Developer",
+      twitter: "https://twitter.com/frank_thi",
+      github: "https://github.com/frathi"
     },
     {
-        name: 'Marc Stammerjohann',
-        picture: 'https://pbs.twimg.com/profile_images/1020673483435540480/haTJfT0O_400x400.jpg',
-        description: 'Full Stack Engineer',
-        twitter: 'https://twitter.com/mrcjln',
-        github: 'https://github.com/marcjulian'
+      name: "Marc Stammerjohann",
+      picture: "https://marcjulian.de/img/me.jpg",
+      description: "Full Stack Engineer",
+      twitter: "https://twitter.com/mrcjln",
+      github: "https://github.com/marcjulian"
     }
-];
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
