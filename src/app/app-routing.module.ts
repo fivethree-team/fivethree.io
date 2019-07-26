e9c2dfa7302fd94ccb686f09d36f4ea370764dbc
Marc Stammerjohann <marcstammerjohann@web.de>
2019-07-26 13:15:53 +0200

    chore(): update to angular 8

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,8 +2,8 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', loadChildren: './pages/home/home.module#HomePageModule' },
-  { path: '**', loadChildren: './pages/page-not-found/page-not-found.module#PageNotFoundPageModule' },
+  { path: '', loadChildren: () => import('./pages/home/home.module').then(m => m.HomePageModule) },
+  { path: '**', loadChildren: () => import('./pages/page-not-found/page-not-found.module').then(m => m.PageNotFoundPageModule) },
 ];
 
 @NgModule({
